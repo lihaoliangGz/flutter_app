fc91e1106e4ac44081f0099178e79ea75a15e248
lihaoliang <cvbmgh@126.com>
2020-04-28 18:21:38 +0800

    save

--- lib/main.dart
+++ lib/main.dart
@@ -140,7 +140,49 @@ class _CounterState extends State<Counter> {
   }
 }
 
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    Widget titleSection = new Container(
+      padding: const EdgeInsets.all(32.0),
+      child: new Row(
+        children: [
+          new Expanded(
+            child: new Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                new Container(
+                  padding: const EdgeInsets.only(bottom: 8.0),
+                  child: new Text(
+                    'Oeschinen Lake Campground',
+                    style: new TextStyle(
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+                new Text(
+                  'Kandersteg, Switzerland',
+                  style: new TextStyle(
+                    color: Colors.grey[500],
+                  ),
+                ),
+              ],
+            ),
+          ),
+          new Icon(
+            Icons.star,
+            color: Colors.red[500],
+          ),
+          new Text('41'),
+        ],
+      ),
+    );
+    //...
+    return titleSection;
+  }
+}
+
 void main() => runApp(new MaterialApp(
       title: 'My app',
-      home: new Counter(),
+      home: new MyApp(),
     ));
