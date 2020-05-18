db1b46d6ab4ea127698772214595dfc1a23dc478
lihaoliang <cvbmgh@126.com>
2020-05-18 14:17:59 +0800

    cookbook-处理手势

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
+import 'package:flutterapp/GestureApp.dart';
 import 'package:flutterapp/ListApp.dart';
 import 'package:flutterapp/WidgetFramework.dart';
 
 void main() {
   debugPaintSizeEnabled = false;
-  runApp(new WidgetFrameworkApp());
+  runApp(new GestureApp());
 }
