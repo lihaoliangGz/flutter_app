8ef2845dacb48d736b08110eba122a0b8b123a12
lihaoliang <cvbmgh@126.com>
2020-05-21 14:26:40 +0800

    输入/打开浏览器

---
+++ lib/TextInputApp.dart
@@ -0,0 +1,60 @@
+import 'package:flutter/material.dart';
+
+class ExampleWidget extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return new ExampleWidgetState();
+  }
+}
+
+class ExampleWidgetState extends State<ExampleWidget> {
+  @override
+  Widget build(BuildContext context) {
+    final TextEditingController _controller = new TextEditingController();
+
+    return new Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: <Widget>[
+        new TextField(
+          onChanged: (value) {
+            print("value = $value");
+          },
+          controller: _controller,
+          decoration: new InputDecoration(hintText: "请输入内容"),
+        ),
+        new RaisedButton(
+          onPressed: () {
+            showDialog(
+                context: context,
+                child: new AlertDialog(
+                  title: new Text("What you typed"),
+                  content: new Text(_controller.text),
+                ));
+          },
+          child: new Text("Done"),
+        )
+      ],
+    );
+  }
+
+  @override
+  void dispose() {
+    print("Dispose 销毁");
+    super.dispose();
+  }
+}
+
+class TextInputApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return new MaterialApp(
+      title: "文本输入",
+      home: new Scaffold(
+        appBar: new AppBar(
+          title: new Text('文本输入'),
+        ),
+        body: new ExampleWidget(),
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
-import 'package:flutterapp/FontsFamilyApp.dart';
-import 'package:flutterapp/NetworkApp.dart';
+import 'package:flutterapp/UrlLauncherApp.dart';
 
 void main() {
   debugPaintSizeEnabled = false;
-  runApp(new FontsFamilyApp());
+  runApp(new UrlLauncherApp());
 }
