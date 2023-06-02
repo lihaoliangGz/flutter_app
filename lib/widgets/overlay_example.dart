e4e7c39850c98a5b8a6af5de139184f2dfef4049
cvbmgh <huangxuanhua@kcreditquantum.com>
2023-06-02 11:53:36 +0800

    overlay

---
+++ lib/widgets/overlay_example.dart
@@ -0,0 +1,65 @@
+import 'package:flutter/material.dart';
+import 'package:flutterapp/base/BaseStatelessApp.dart';
+
+///使用StatefulWidget管理状态会更好
+class OverlayExample extends BaseStatelessApp {
+  OverlayEntry? _overlayEntry;
+
+  @override
+  Widget getBody() {
+    return ListView(
+      children: [
+        ElevatedButton(
+          onPressed: () {
+            _createHighLightOverlay();
+          },
+          child: Text(
+            "show",
+            style: TextStyle(color: Colors.white),
+          ),
+        ),
+      ],
+    );
+  }
+
+  void _createHighLightOverlay() {
+    _overlayEntry = OverlayEntry(
+      builder: (context) {
+        return SafeArea(
+          //使用DefaultTextStyle, 弥补没有Scaffold的问题,不然文本底部会出现双划线
+          child: DefaultTextStyle(
+            style: TextStyle(
+              color: Colors.blue,
+              fontWeight: FontWeight.bold,
+              fontSize: 14.0,
+            ),
+            child: GestureDetector(
+              onTap: () {
+                _removeHighLightOverlay();
+              },
+              child: Container(
+                color: Colors.black12,
+                width: 200,
+                height: 100,
+                child: Center(
+                  child: Text("float dialog"),
+                ),
+              ),
+            ),
+          ),
+        );
+      },
+    );
+    Overlay.of(context).insert(_overlayEntry!);
+  }
+
+  void _removeHighLightOverlay() {
+    _overlayEntry?.remove();
+    _overlayEntry == null;
+  }
+
+  @override
+  String getTitle() {
+    return "Overlay";
+  }
+}
