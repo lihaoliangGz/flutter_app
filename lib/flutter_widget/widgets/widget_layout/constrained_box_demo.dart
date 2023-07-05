9e5d056a6557bdd9b09656fc4d3421e19fa52cfd
cvbmgh <huangxuanhua@kcreditquantum.com>
2023-07-05 12:25:20 +0800

    ConstrainedBox

--- lib/flutter_widget/widgets/widget_layout/constrained_box_demo.dart
+++ lib/flutter_widget/widgets/widget_layout/constrained_box_demo.dart
@@ -1,16 +1,32 @@
 import 'package:flutter/material.dart';
-
 import 'package:flutterapp/base/BaseStatelessApp.dart';
 
 class ConstrainedBoxDemo extends BaseStatelessApp {
   @override
   Widget getBody() {
-    return ConstrainedBox(
-      constraints: BoxConstraints(
-          minWidth: 80, minHeight: 80, maxWidth: 80, maxHeight: 80),
-      child: Card(
-        child: Text("Hello World!"),
-      ),
+    return Column(
+      children: [
+        ConstrainedBox(
+          constraints: BoxConstraints(
+              minWidth: 80, minHeight: 80, maxWidth: 80, maxHeight: 80),
+          child: Card(
+            child: Text("Hello World!"),
+          ),
+        ),
+        Container(
+          height: 200,
+          child: ConstrainedBox(
+            constraints: BoxConstraints.expand(),
+            child: const Card(child: Text('Hello World!')),
+          ),
+        ),
+        Container(
+          height: 200,
+          child: SizedBox.expand(
+            child: const Card(child: Text('Hello World!')),
+          ),
+        ),
+      ],
     );
   }
 
