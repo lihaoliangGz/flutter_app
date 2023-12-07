719b80c09b00b6d7d756bb8895ac15391037efd7
cvbmgh <cvbmgh@126.com>
2023-12-07 20:34:18 +0800

    upgrade

--- lib/flutter_widget/widgets/touch_interactions/touch_interaction_app.dart
+++ lib/flutter_widget/widgets/touch_interactions/touch_interaction_app.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutterapp/base/BaseStatelessApp.dart';
+import 'package:flutterapp/flutter_widget/widgets/touch_interactions/draggable_example.dart';
 import 'package:flutterapp/flutter_widget/widgets/touch_interactions/draggable_scrollable_sheet_example.dart';
 
 class TouchInteractionApp extends BaseStatelessApp {
@@ -14,6 +15,16 @@ class TouchInteractionApp extends BaseStatelessApp {
               context,
               MaterialPageRoute(
                   builder: (context) => DraggableScrollableSheetExample()),
+            );
+          },
+        ),
+        ListTile(
+          title: Text("Draggable"),
+          onTap: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(
+                  builder: (context) => DraggableExample()),
             );
           },
         ),
